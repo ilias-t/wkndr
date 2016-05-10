e89bf7ea328a23f7cde62968f9d8a95727cc8517
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-05-10 08:58:59 -0700

    add api key comment

--- app/models/campsite.rb
+++ app/models/campsite.rb
@@ -1,5 +1,6 @@
 class Campsite < ActiveRecord::Base
   def self.search(query)
+    # register for API keys: http://developer.active.com/apps/myapps
     p "http://api.amp.active.com/camping/campgrounds?pstate=CA&pname=#{query}&api_key=#{ENV['CAMPGROUND_API_KEY']}"
   end
 end

70c7e8fdd2cde960967ed311db7a0ffdb2f7751b
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-05-10 08:56:20 -0700

    add

--- app/models/campsite.rb
+++ app/models/campsite.rb
@@ -1,2 +1,5 @@
 class Campsite < ActiveRecord::Base
+  def self.search(query)
+    p "http://api.amp.active.com/camping/campgrounds?pstate=CA&pname=#{query}&api_key=#{ENV['CAMPGROUND_API_KEY']}"
+  end
 end

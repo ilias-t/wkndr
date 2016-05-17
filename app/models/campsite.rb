45735d87f1f8213b61c7513feac83a0e73645da5
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-05-17 14:27:42 -0700

    search for campsites, get back xml

--- app/models/campsite.rb
+++ app/models/campsite.rb
@@ -1,6 +1,9 @@
 class Campsite < ActiveRecord::Base
   def self.search(query)
     # register for API keys: http://developer.active.com/apps/myapps
-    p "http://api.amp.active.com/camping/campgrounds?pstate=CA&pname=#{query}&api_key=#{ENV['CAMPGROUND_API_KEY']}"
+    base_domain = "http://api.amp.active.com/camping/campgrounds"
+    end_point = URI(base_domain + "?pstate=CA&pname=#{query}&api_key=#{ENV['CAMPGROUND_API_KEY']}")
+    response = Net::HTTP.get(end_point)
+    p response
   end
 end

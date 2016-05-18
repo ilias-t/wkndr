30e627b74d8e57fd67ced6fc1c3d4b285af95fb8
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-05-17 17:19:08 -0700

    getting partials to work in handlebars...

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -4,7 +4,12 @@ var wkndr = {
     var campsiteName = e.target.querySelector("#campsite-name").value;
 
     $.get("/campsites?campsite=" + campsiteName, function(res) {
-      console.log("res:", res);
+      // determine if res is a single object or a collection
+      data = res;
+      var html = res.constructor === Array ?
+         HandlebarsTemplates['campsites/index']({campsites: res}) :
+         HandlebarsTemplates['campsites/show']({campsite: res});
+      $("#campsites-container").html(html)
     })
   }
 }
--- app/controllers/campsites_controller.rb
+++ app/controllers/campsites_controller.rb
@@ -2,7 +2,6 @@ class CampsitesController < ApplicationController
   def index
     #TODO: respond to html requests with page & json requests with data
     if is_xrh_request # inherited from application controller
-      binding.pry
       campsites = Campsite.search(params[:campsite])
       render json: campsites
     else

8da4c4787cc6e99584fbff892beba5b6df2b804e
Ilias Tsangaris <iliastsangaris@gmail.com>
2016-05-17 23:05:16 -0700

    templates both arrays & objects from campsite response

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,28 +3,21 @@ var wkndr = {
   searchCampsites: function(e) {
     e.preventDefault()
     const campsiteName = e.target.querySelector("#campsite-name").value // grab campsite name from input
-
     $.get("/campsites?campsite=" + campsiteName, this.renderCampsites)
 
   },
 
   renderCampsites: function(response) { // xhr request
-    window.html = new String()
-    window.data = response
-
-    if (response.constructor === Array) { // determine if res is a single object or a collection
+    let html = new String()
+    if (response instanceof Array) { // determine if res is a single object or a collection
       const campsites = response
       campsites.forEach(function(campsite) {
         html += HandlebarsTemplates['campsites/show'](campsite) // current solution as handelbars partials don't enjoy the asset pipeline
       })
-    } else {
+    } else if (response instanceof Object) {
       const campsite = response
       html = HandlebarsTemplates['campsites/show'](campsite)
-    }
-
-    // const html = res.constructor === Array ?
-    //    HandlebarsTemplates['campsites/index']({campsites: res}) :
-    //    HandlebarsTemplates['campsites/show']({campsite: res});
+    } else { html = "Try searching for something different" }
     $("#campsites-container").html(html)
   }
 
